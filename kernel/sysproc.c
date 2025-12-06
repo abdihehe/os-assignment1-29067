1080d38664bd7089cf6a48c942b4b739537b8a91
abdullah <syedabdullah1722@gmail.com>
2025-12-07 01:25:07 +0500

    pset2

--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -7,6 +7,7 @@
 #include "proc.h"
 #include "vm.h"
 
+
 uint64
 sys_exit(void)
 {
@@ -53,7 +54,7 @@ sys_sbrk(void)
     }
   } else {
     // Lazily allocate memory for this process: increase its memory
-    // size but don't allocate memory. If the processes uses the
+    // size but don't allocate memory. If the process uses the
     // memory, vmfault() will allocate it.
     if(addr + n < addr)
       return -1;
@@ -104,4 +105,29 @@ sys_uptime(void)
   xticks = ticks;
   release(&tickslock);
   return xticks;
+}
+
+// interpose(mask, path)
+uint64
+sys_interpose(void)
+{
+  int mask;
+  char buf[512]; // buffer for the path string
+
+  // 1) Retrieve the integer mask
+  argint(0, &mask);
+
+  // 2) Retrieve the string path into buf
+  if (argstr(1, buf, sizeof(buf)) < 0)
+    return -1;
+
+  // 3) Store in current process
+  struct proc *p = myproc();
+  p->interpose_mask = (uint)mask;
+
+  // 4) Save allowed path safely
+  strncpy(p->allowed_path, buf, sizeof(p->allowed_path) - 1);
+  p->allowed_path[sizeof(p->allowed_path) - 1] = '\0';
+
+  return 0;
 }
